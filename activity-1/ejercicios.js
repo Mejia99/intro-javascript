546c889b704cfd95cf84f60185c1e9979499059d
Mejia99 <alejandrmejia955@gmail.com>
2022-07-05 14:23:38 -0500

    Actividad 1

---
+++ activity-1/ejercicios.js
@@ -0,0 +1,27 @@
+//Ejercicio 1.-
+//Multiplicacion de cualquier numero por 2
+
+console.log("Ingrese un numero"), prompt()*2;
+
+
+//Ejercicio 2
+//Resta de dos numero
+
+var num1 = prompt("Ingrese un numero");
+var num2 = prompt("Ingrese otro numero");
+console.log(num1-num2);
+
+
+//Ejercicio 3
+//Calcular el radio de un circulo
+
+var r = 5.3;
+var pi=3.1416;
+console.log(pi*r^2);
+
+//Ejercicio 4
+//calcular el area de un rombo
+
+var D = 8;
+var d=6;
+console.log(D*d/2)
