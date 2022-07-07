d70dabc23d01af3f12e90cec738e329d477d5f80
Mejia99 <alejandrmejia955@gmail.com>
2022-07-07 17:47:35 -0500

    Actividad 3

---
+++ activity-3/ejercicios.js
@@ -0,0 +1,48 @@
+//Ejercicio #1
+
+var edad = (prompt("¿Cuál es tu edad?"));
+if(edad >= 18){
+    alert("Puedes Ingresar a la Discoteca");
+}else{
+    alert("No puedes ingresar a la Discoteca");
+}
+
+
+//Ejercicio #3
+
+var numero = (prompt("Ingresa un numero"));
+if(numero % 2=== 0){
+    alert(numero + " Es divible entre 2");
+}else{
+    alert(numero + " No es divisible entre 2");
+}
+
+
+//Ejercicio #4
+
+var numero = (prompt("Ingresa un numero"));
+if(numero % 2=== 0){
+    alert(" Es Par el " + numero);
+}else{
+    alert(" No es Par el " + numero);
+}
+
+//Ejercicio 5
+ 
+var numero = (prompt("Ingresa un numero"));
+if(numero ==1000){
+    alert( " Ganaste un premio");
+}else{
+    alert(" Lo sentimos sigue participando");
+}
+
+//Ejercicio 6
+var numero1 = (prompt("Ingresa un numero"));
+var numero2 = (prompt("Ingresa un numero"));
+if(numero1>= numero2){
+    alert(numero1 + " Es mayor que" + numero2);
+}else{
+    alert(numero2 + " Es mayor que " + numero1);
+}
+
+//Ejercicio 7
